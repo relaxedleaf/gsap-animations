4d7a180b17830337ad8cdd35b5496c926feb0669
Guanghui Li <liguanghui.cs@gmail.com>
2022-10-02 20:52:30 -0400

    text animations

--- text_reveal/app.js
+++ text_reveal/app.js
@@ -20,3 +20,19 @@ tl.fromTo('.cta4', { x: '100%', opacity: 0.5 }, { x: 0, opacity: 1 }, '<20%');
 tl.fromTo('.cta6', { x: '-100%', opacity: 0.5 }, { x: 0, opacity: 1 }, '<20%');
 tl.fromTo('.cta5', { y: '100%', opacity: 0.5 }, { y: 0, opacity: 1 }, '<20%');
 tl.fromTo('.cta-btn', { y: 20, opacity: 0 }, { y: 0, opacity: 1 }, '<');
+
+//Split text alternative
+const logo = document.querySelector('.logo');
+const letters = logo.textContent.split('');
+
+logo.textContent = '';
+
+letters.forEach((letter) => {
+	logo.innerHTML += `<span class="letter">${letter}</span>`;
+});
+gsap.set('.letter', { display: 'inline-block' });
+gsap.fromTo(
+	'.letter',
+	{ y: '100%' },
+	{ y: 0, delay: 2, stagger: 0.05, ease: 'back.out(3)' }
+);
